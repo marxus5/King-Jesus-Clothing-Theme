ed8d9ff06e138e5b5326e7bb097e06123a5dfec3
marxus5 <surjopolosii@gmail.com>
2026-02-21 15:23:37 -0500

    updated functions.php to enqueue styles with file modification time for cache

--- functions.php
+++ functions.php
@@ -133,7 +133,14 @@ add_filter( 'woocommerce_update_order_review_fragments', function( $fragments ) 
 
 
 function printshop_enqueue_styles() {
-    wp_enqueue_style('main-style', get_stylesheet_uri());
+
+    wp_enqueue_style(
+        'main-style',
+        get_stylesheet_uri(),
+        array(),
+        filemtime(get_stylesheet_directory() . '/style.css')
+    );
+
 }
 add_action('wp_enqueue_scripts', 'printshop_enqueue_styles');
 
