bb36b71548d8accffe84b614ebd77f02aa998810
marxus5 <81498254+marxus5@users.noreply.github.com>
2026-09-01 21:10:23 -0400

    Update shipping information in single-product.php

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -1316,7 +1316,7 @@ ul.products li.product button.button:hover {
                     </div>
                     <div class="custom-trust-item">
                         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"/></svg>
-                        Free Shipping $80+
+                        Free US Shipping $99+
                     </div>
                     <div class="custom-trust-item">
                         <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><path d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/></svg>
